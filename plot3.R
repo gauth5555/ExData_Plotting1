1a25b486156a20ba124db504b756da8b6c4baca0
gauth5555 <gmanghnani@argusinformation.com>
2017-08-20 16:50:43 -0400

    Week 1 Assignment - Exploratory Analysis

---
+++ plot3.R
@@ -0,0 +1,57 @@
+
+##About
+#This was the first project for the Exploratory Data Analysis course in Coursera's Data Science specialization track. The objective was to practice creating plots by reproducing a series of four plots, and also to practice using github by forking and cloning an existing repository.
+
+##Sypnosis
+#The goal here was simply to examine how household energy usage varies over a 2-day period in February, 2007 by reproducing a series of four plots. Due to specific image size requirements of the new plots, they will look slightly different than the originals.
+
+##Data
+#This project used data from the UC Irvine Machine Learning Repository, a popular repository for machine learning datasets. The "Individual household electric power consumption Data Set" was used and made available on the course web site:
+
+##Electric power consumption Dataset (20Mb)
+
+##Description: Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+
+#The following descriptions of the 9 variables in the dataset are taken from the UCI web site
+
+#Date: Date in format dd/mm/yyyy
+#Time: time in format hh:mm:ss
+#Global_active_power: household global minute-averaged active power (in kilowatt)
+#Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+#Voltage: minute-averaged voltage (in volt)
+#Global_intensity: household global minute-averaged current intensity (in ampere)
+#Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+#Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+#Data Loading and Preprocessing
+
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+
+
+### Plot 3
+
+plot3 <- function() {
+  plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(df$timestamp,df$Sub_metering_2,col="red")
+  lines(df$timestamp,df$Sub_metering_3,col="blue")
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+  dev.copy(png, file="plot3.png", width=480, height=480)
+  dev.off()
+}
+plot3()
